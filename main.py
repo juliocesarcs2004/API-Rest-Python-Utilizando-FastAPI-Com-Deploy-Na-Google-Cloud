defcd41b215c918e77dacdcac9ed262923d2ca5a
Julio <juliocesarcs2004@gmail.com>
2024-10-21 12:12:40 -0300

    Criado o model e finalizada a configuracao do ORM com teste em banco local

--- main.py
+++ main.py
@@ -2,13 +2,13 @@ import uvicorn
 from fastapi import FastAPI
 
 from contas_a_pagar_e_receber.routers import contas_a_pagar_e_receber_router
-from shared.database import engine, Base
+# from shared.database import engine, Base
 
 
-from contas_a_pagar_e_receber.models.conta_a_pagar_e_receber_model import ContaPagarReceber
-
-Base.metadata.drop_all(bind=engine)
-Base.metadata.create_all(bind=engine)
+# from contas_a_pagar_e_receber.models.conta_a_pagar_e_receber_model import ContaPagarReceber
+#
+# Base.metadata.drop_all(bind=engine)
+# Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
