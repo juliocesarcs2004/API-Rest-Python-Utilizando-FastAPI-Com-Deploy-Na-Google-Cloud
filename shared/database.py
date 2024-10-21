a6596c2be55875920155f2a168eb872bfdd7be0a
Julio <juliocesarcs2004@gmail.com>
2024-10-21 11:28:26 -0300

    Criado um modelo de dados e corrigida a string de conexao

--- shared/database.py
+++ shared/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 # SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db."
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:1234@localhost/db_fast_api_zero_ate_deploy."
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:1234@localhost/db_fast_api_zero_ate_deploy"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL
