8593dc0e634193b94f746da9be010d243dc405ba
Julio Cesar <julio.cordeiro.prestserv@petrobras.com.br>
2024-12-03 09:12:37 -0300

    Criada API para consulta de um determinado fornecedor cliente junto com seus respectivos testes

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import uvicorn
-from fastapi import FastAPI, Request
-from fastapi.responses import JSONResponse
+from fastapi import FastAPI
 
-from contas_a_pagar_e_receber.routers import contas_a_pagar_e_receber_router, fornecedor_cliente_router
+from contas_a_pagar_e_receber.routers import contas_a_pagar_e_receber_router, fornecedor_cliente_router, \
+    fornecedor_cliente_vs_contas_router
 from shared.exceptions import NotFound
 from shared.exceptions_handler import not_found_exception_handler
 
@@ -13,11 +13,11 @@ app = FastAPI()
 def oi_eu_sou_programador() -> str:
     return "Oi, eu sou programador!"
 
+
 app.include_router(contas_a_pagar_e_receber_router.router)
 app.include_router(fornecedor_cliente_router.router)
+app.include_router(fornecedor_cliente_vs_contas_router.router)
 app.add_exception_handler(NotFound, not_found_exception_handler)
-
-
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8001)
